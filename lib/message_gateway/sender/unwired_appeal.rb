b1e3c9f84f7254715772eccbd61201139c7f99c7
Joshua Hull <joshbuddy@gmail.com>
2010-09-07 10:47:25 -0400

    test carrier method on message

--- lib/message_gateway/sender/unwired_appeal.rb
+++ lib/message_gateway/sender/unwired_appeal.rb
@@ -7,7 +7,7 @@ class MessageGateway
 
       attr_accessor :event_id, :password
       def send(message)
-        defer_success_on_200(EM::HttpRequest.new('http://appsrv.unwiredappeal.com/uwa_umap/eventhandler.srv').get :query => { 'evid' => @event_id, 'pw' => @password, 'carrier' => message.carrier ? denormalize_carrier(message.carrier) : '', 'msg' => message.body, 'number' => canonicalize_phone_number(message.to) })
+        defer_success_on_200(EM::HttpRequest.new('http://appsrv.unwiredappeal.com/uwa_umap/eventhandler.srv').get :query => { 'evid' => @event_id, 'pw' => @password, 'carrier' => message.respond_to?(:carrier) && message.carrier ? denormalize_carrier(message.carrier) : '', 'msg' => message.body, 'number' => canonicalize_phone_number(message.to) })
       end
 
       def verify
