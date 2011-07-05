3f7eff9de06c90ccf63b2f44e3f012db7b46a281
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-07-05 13:14:07 -0700

    update mobile messenger code per new API specs

--- lib/message_gateway/sender/mobile_messenger.rb
+++ lib/message_gateway/sender/mobile_messenger.rb
@@ -3,7 +3,9 @@ class MessageGateway
     class MobileMessenger < Sender
       attr_accessor :user, :password, :shortcode
       def send(message)
-        defer_success_on_200(EM::HttpRequest.new("https://#{@user}:#{@password}@sendsms.mobilecdn.verisign.com/wsgw/sendSingle").post :data => { 'message' => message.body, 'serviceCode' => @shortcode, 'destination' => canonicalize_phone_number(message.to) })
+        defer_success_on_200(  EM::HttpRequest.new("https://sendsms.mobilemessenger.com/wsgw/sendSingle").post(
+          :data => { 'message' => message.body, 'serviceCode' => @shortcode, 'destination' => canonicalize_phone_number(message.to) },
+          :head => {'authorization' => [@user, @password]} )  )
       end
 
       def verify
