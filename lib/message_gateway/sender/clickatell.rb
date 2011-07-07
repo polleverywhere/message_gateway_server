2b8662fd43d82ecfeab7ca6cb86a701bc9c925be
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-07-06 17:37:29 -0700

    wrap lines

--- lib/message_gateway/sender/clickatell.rb
+++ lib/message_gateway/sender/clickatell.rb
@@ -3,7 +3,9 @@ class MessageGateway
     class Clickatell < Sender
       attr_accessor :api_id, :login, :password
       def send(message)
-        defer_success_on_200(EM::HttpRequest.new('http://api.clickatell.com/http/sendmsg').get :query => { 'api_id' => @api_id, 'user' => @login, 'password' => @password, 'mo' => '1', 'from' => message.from, 'to' => message.to, 'text' => message.body })
+        defer_success_on_200(  EM::HttpRequest.new('http://api.clickatell.com/http/sendmsg').get(
+          :query => { 'api_id' => @api_id, 'user' => @login, 'password' => @password, 'mo' => '1',
+              'from' => message.from, 'to' => message.to, 'text' => message.body } )  )
       end
 
       def verify
