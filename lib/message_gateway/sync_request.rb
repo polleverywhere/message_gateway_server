9928fe88737c16a61c334447bde6bef610414c1b
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-07-11 16:01:02 -0700

    sync request now works correctly

--- lib/message_gateway/sync_request.rb
+++ lib/message_gateway/sync_request.rb
@@ -2,12 +2,19 @@ class MessageGateway
   class SyncRequest
     def post(send_url, post_params)
       url = URI.parse(send_url)
-      Net::HTTP.start(url.host, url.port) do |http|
-        req = Net::HTTP.Post.new(url.path)
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true if send_url =~ /https/
+
+      http.start() do |http|
+        puts "about to send to: #{url.path}"
+        puts "posting: #{post_params[:data]}"
+
+        req = Net::HTTP::Post.new(url.path)
         if post_params[:head]
-          req.basic_auth post_params[:head]['authorization'][[0], post_params[:head]['authorization'][[1]
+          req.basic_auth post_params[:head]['authorization'][0], post_params[:head]['authorization'][1]
         end
-        req.set_form_data( post_params[:data], ';' )
+
+        req.set_form_data( post_params[:data] )
         response = http.request(req)
 
         print response.body  # TODO: something better here
