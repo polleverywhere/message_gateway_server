d3c0f916c122e1f4764c0c739e4f2602f990d0e4
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-07-19 14:33:54 -0700

    Twilio sender now has a default from parameter

--- lib/message_gateway/sender/twilio.rb
+++ lib/message_gateway/sender/twilio.rb
@@ -9,14 +9,14 @@ class MessageGateway
 
       API_VERSION = '2010-04-01'
 
-      attr_accessor :account_sid, :account_token
+      attr_accessor :account_sid, :account_token, :default_from
 
       def send(message)
         twilio_send_url = "https://api.twilio.com/#{API_VERSION}/Accounts/#{@account_sid}/SMS/Messages"
 
-        request_object.post( twilio_send_url, 
+        request_object.post( self, twilio_send_url, 
           :data => { 'Body' => message.body, 
-              'From' => message.from, 'To' => canonicalize_phone_number(message.to) },
+              'From' => message.from || default_from, 'To' => canonicalize_phone_number(message.to) },
           :head => {'authorization' => [@account_sid, @account_token]} )
       end
 
