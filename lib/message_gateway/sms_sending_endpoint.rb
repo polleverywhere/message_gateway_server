e38229da670abda5ad35e016ce601620aa781755
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-07-25 06:55:01 -0700

    making errors that came from message gateway, vs the caller, obvious. This is to help figure where the freak error X is from

--- lib/message_gateway/sms_sending_endpoint.rb
+++ lib/message_gateway/sms_sending_endpoint.rb
@@ -33,7 +33,7 @@ class MessageGateway
       req['source'] ||= @default_source
 
       unless source = @gateway.dispatchers[req['source']]
-        return Rack::Response.new(["The source '#{req['source']}' could not be recognized. Source must be one of the following:\n#{@gateway.dispatchers.keys.join("\n")}"], 400).finish
+        return Rack::Response.new(["MEASSAGE GATEWAY: The source '#{req['source']}' could not be recognized. Source must be one of the following:\n#{@gateway.dispatchers.keys.join("\n")}"], 400).finish
       end
 
       req['from'] ||= source.out.default_from
@@ -41,7 +41,7 @@ class MessageGateway
       # parameter checking
       %w(source body to).each do |p|
         if !req.params.key?(p)
-          return Rack::Response.new(["The parameter '#{p}' must be supplied."], 400).finish
+          return Rack::Response.new(["MEASSAGE GATEWAY: The parameter '#{p}' must be supplied."], 400).finish
         end
       end
 
@@ -49,14 +49,14 @@ class MessageGateway
         message_delay = begin 
           req['intra_message_delay'] ? Integer(req['intra_message_delay']) : INTRA_MESSAGE_DELAY
         rescue ArgumentError
-          return Rack::Response.new(["intra_message_delay must be an int."], 400).finish
+          return Rack::Response.new(["MEASSAGE GATEWAY: intra_message_delay must be an int."], 400).finish
         end
 
         begin
           data = {'from' => sanitize_phone_number(req['from']), 'to' => sanitize_phone_number(req['to']), 'source' => req['source']}
           data['carrier_id'] = req['carrier_id'] if req['carrier_id']
           if data['from'].empty?
-            Rack::Response.new(["The parameter 'from' must be supplied."], 400).finish
+            Rack::Response.new(["MEASSAGE GATEWAY: The parameter 'from' must be supplied."], 400).finish
           else
             bodies = begin
               Array(JSON.parse(req['body']))
@@ -77,7 +77,7 @@ class MessageGateway
       end
     rescue
       @gateway.log.error "#{$!.message}\n#{$!.backtrace.join("\n  ")}"
-      Rack::Response.new(['There was a problem'], 500).finish
+      Rack::Response.new(['MEASSAGE GATEWAY: There was a problem'], 500).finish
     end
   end
 end
