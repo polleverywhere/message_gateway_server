aec38da9e941c8bf4a2b2b8cd9ffe2520d4af20f
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-10-24 07:25:00 -0700

    updating carrier mappings

--- lib/message_gateway/parser/twilio.rb
+++ lib/message_gateway/parser/twilio.rb
@@ -1,7 +1,11 @@
 class MessageGateway
   module Parser
     class Twilio < Simple
-    
+
+      # NOTE: Twilio does not have access to carrier code information as of this writing.
+      # Source: <http://getsatisfaction.com/twilio/topics/how_to_get_cell_phone_carrier_via_sms>
+      # WD-rpw 10-10-2011
+
       def body_value(req)
         req.params['Body']
       end
