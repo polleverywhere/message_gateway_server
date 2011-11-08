5e523e3b01021bceb75682e9b03ae26f3922bc02
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-11-08 11:35:43 -0800

    adding debugging statement, to be removed later

--- lib/message_gateway/sms_message.rb
+++ lib/message_gateway/sms_message.rb
@@ -6,17 +6,19 @@ class MessageGateway
       h["carrier_id"] = carrier_id
       h
     end
-    
+
     def self.build_from_hash(hash)
+      debugger
+      0
       new_message = super(hash)
       new_message.carrier_id = hash['carrier_id']
       new_message
     end
-    
+
     def extra
       {'carrier_id' => carrier_id}
     end
-    
+
     def carrier
       MessageGateway::Util::Carrier::CarrierInfo.lookup(carrier_id)
     end
