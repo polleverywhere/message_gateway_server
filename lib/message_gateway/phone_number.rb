9ce82ab56280aa7fbd62e9a155ee9ee4efd64725
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-11-14 12:11:29 -0800

    only sanitize the phone number if it exists

--- lib/message_gateway/phone_number.rb
+++ lib/message_gateway/phone_number.rb
@@ -1,20 +1,20 @@
 class MessageGateway
   module PhoneNumber
     def sanitize_phone_number(phone)
-      sanitized_phone = phone.gsub(/\D/, '')
+      sanitized_phone = phone.gsub(/\D/, '') if phone
       sanitized_phone.empty? ? phone : sanitized_phone
     end
 
     def sanitize_us_phone_number(phone)
       sanitize_phone_number(phone)[/^1?([^10]\d{9})$/, 1] || ""
     end
-    
+
     def canonicalize_phone_number(phone)
       phone = sanitize_phone_number(phone)
       phone[0,0] = '1' if phone.size == 10
       phone.blank? ? nil : phone
     end
-    
+
   end
 
 end
