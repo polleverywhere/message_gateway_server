260bfd767f770417f966fef4744c1e59765fec86
Ryan Wilcox <rwilcox@wilcoxd.com>
2011-12-26 14:32:17 -0800

    fix spelling of messenger

--- lib/message_gateway/sender.rb
+++ lib/message_gateway/sender.rb
@@ -26,16 +26,16 @@ class MessageGateway
   class Sender
     include Logging
 
-    autoload :Celltrust,     'message_gateway/sender/celltrust'
-    autoload :Clickatell,    'message_gateway/sender/clickatell'
-    autoload :Mblox,         'message_gateway/sender/mblox'
-    autoload :MobileMessager, 'message_gateway/sender/mobile_messenger'
-    autoload :MxTelecom,     'message_gateway/sender/mx_telecom'
-    autoload :Opera,         'message_gateway/sender/opera'
-    autoload :Smtp,          'message_gateway/sender/smtp'
-    autoload :Twilio,        'message_gateway/sender/twilio'
-    autoload :Txtnation,     'message_gateway/sender/txtnation'
-    autoload :UnwiredAppeal, 'message_gateway/sender/unwired_appeal'
+    autoload :Celltrust,      'message_gateway/sender/celltrust'
+    autoload :Clickatell,     'message_gateway/sender/clickatell'
+    autoload :Mblox,          'message_gateway/sender/mblox'
+    autoload :MobileMessenger,'message_gateway/sender/mobile_messenger'
+    autoload :MxTelecom,      'message_gateway/sender/mx_telecom'
+    autoload :Opera,          'message_gateway/sender/opera'
+    autoload :Smtp,           'message_gateway/sender/smtp'
+    autoload :Twilio,         'message_gateway/sender/twilio'
+    autoload :Txtnation,      'message_gateway/sender/txtnation'
+    autoload :UnwiredAppeal,  'message_gateway/sender/unwired_appeal'
 
     attr_accessor :name, :from, :default_from, :request_style
 
