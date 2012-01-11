50cae65445a4023132b838539dab13a5b9c0598e
Ryan Wilcox <rwilcox@wilcoxd.com>
2012-01-10 16:07:35 -0800

    add temp debugging statement

--- lib/message_gateway/parser/simple_carrier.rb
+++ lib/message_gateway/parser/simple_carrier.rb
@@ -10,6 +10,9 @@ class MessageGateway
 
       def call(env)
         req = Rack::Request.new(env)
+        logger = MessageGateway.default_logger
+
+        logger.error req.inspect
         build_and_dispatch(
           sanitize_phone_number(from_value(req)),
           sanitize_phone_number(to_value(req)) || default_to,
