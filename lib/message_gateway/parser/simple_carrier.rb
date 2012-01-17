0df5c242bcceef3ad7a74f9606084dd1d9977b31
Zach Zolton <zachary.zolton@gmail.com>
2012-01-17 14:35:10 -0600

    keep updating logging...

--- lib/message_gateway/parser/simple_carrier.rb
+++ lib/message_gateway/parser/simple_carrier.rb
@@ -12,7 +12,7 @@ class MessageGateway
         req = Rack::Request.new(env)
 
         MessageGateway::SysLogger.info "The incoming request: #{req.inspect}"
-        MessageGateway::SysLogger.info "With body:\n#{req.body}"
+        MessageGateway::SysLogger.info "With body:\n#{req.body.read}"
 
         build_and_dispatch(
           sanitize_phone_number(from_value(req)),
