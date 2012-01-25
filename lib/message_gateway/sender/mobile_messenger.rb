8bdf66779e33d2b7ba9e04913eca348960601a25
Zach Zolton <zachary.zolton@gmail.com>
2012-01-25 11:10:13 -0600

    ensure we mixin the PhoneNumber module for the MobileMessenger sender

--- lib/message_gateway/sender/mobile_messenger.rb
+++ lib/message_gateway/sender/mobile_messenger.rb
@@ -1,11 +1,17 @@
 class MessageGateway
   class Sender
     class MobileMessenger < Sender
+      include PhoneNumber
       attr_accessor :user, :password, :shortcode
+
       def send(message)
-        request_object.post( self, "https://sendsms.mobilemessenger.com/wsgw/sendSingle",
-          :data => { 'message' => message.body, 'serviceCode' => @shortcode, 'destination' => canonicalize_phone_number(message.to) },
-          :head => {'authorization' => [@user, @password]} )
+        request_object.post(self, "https://sendsms.mobilemessenger.com/wsgw/sendSingle",
+          :data => {
+            'message'     => message.body,
+            'serviceCode' => @shortcode,
+            'destination' => canonicalize_phone_number(message.to)
+          },
+          :head => {'authorization' => [@user, @password]})
       end
 
       def verify
