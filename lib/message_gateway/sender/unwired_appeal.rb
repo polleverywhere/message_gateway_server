387f5786a5f1883317134c057690c01ff6470f82
Zach Zolton <zachary.zolton@gmail.com>
2012-01-27 10:49:18 -0600

    make sure unwired appeal sender doesn't try to access a message's carrier for non-SMS messages...

--- lib/message_gateway/sender/unwired_appeal.rb
+++ lib/message_gateway/sender/unwired_appeal.rb
@@ -9,16 +9,15 @@ class MessageGateway
       END_POINT_URL = 'http://appsrv.unwiredappeal.com/uwa_umap/eventhandler.srv'
 
       def send(message)
-        request_object.get(self, END_POINT_URL,
-            :query => {
-                'evid'    => self.event_id,
-                'pw'      => self.password,
-                'carrier' => message.carrier,
-                'number'  => canonicalize_phone_number(message.to),
-                "msg"     => CGI.escape(message.body)
-            }
-        )
+        request_object.get self, END_POINT_URL, :query => {
+          'evid'    => self.event_id,
+          'pw'      => self.password,
+          'carrier' => (message.carrier if message.respond_to? :carrier),
+          'number'  => canonicalize_phone_number(message.to),
+          "msg"     => CGI.escape(message.body)
+        }
       end
     end
+
   end
 end
