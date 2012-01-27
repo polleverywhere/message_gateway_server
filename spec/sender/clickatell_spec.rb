e15cbdf76d3766b7903b95d3440b13707a8be19b
Zach Zolton <zachary.zolton@gmail.com>
2012-01-26 19:44:02 -0600

    just taking a read... (^_^

--- spec/sender/clickatell_spec.rb
+++ spec/sender/clickatell_spec.rb
@@ -2,18 +2,17 @@ require 'spec_helper'
 require 'dirge'
 
 describe MessageGateway::Sender::Clickatell do
-  before(:each) do
+  before :each do
     stub_request(:any,
       "http://api.clickatell.com:80/http/sendmsg?mo=1&api_id=api_id&from=from&text=body&to=to&password=password&user=login")
-      .to_return( :body => file_obj_for('processor/fixtures/twitter/send.txt') )
+      .to_return(:body => file_obj_for('processor/fixtures/twitter/send.txt'))
 
     @sender = MessageGateway::Sender::Clickatell.new
     @sender.init
-    @sender.api_id = 'api_id'
-    @sender.login = 'login'
+    @sender.api_id   = 'api_id'
+    @sender.login    = 'login'
     @sender.password = 'password'
   end
-
 
   it "should send a message" do
     EM.run do
